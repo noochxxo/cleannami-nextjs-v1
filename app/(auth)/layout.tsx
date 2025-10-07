2e792b633a53440dd74f3a4d6484e2e896a5a9d0
Eric Trotchie <ejtrotchie@gmail.com>
2025-10-06 22:15:49 -0600

    added header to auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import { Header } from "@/components/home/Header";
+
 export default function Layout({
   children,
 }: Readonly<{
@@ -5,7 +7,9 @@ export default function Layout({
 }>) {
 
   return (
-    <div className="relative min-h-screen bg-gray-100 flex items-center justify-center antialiased">
+    <>
+      <Header />
+      <div className="relative min-h-screen bg-gray-100 flex items-center justify-center antialiased">
       <div
         className="absolute inset-0 bg-cover bg-center z-0"
         style={{ backgroundImage: "url('https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=2070&auto=format&fit=crop')" }}
@@ -14,5 +18,6 @@ export default function Layout({
       </div>
       {children}
     </div>
+    </>
   );
 };
